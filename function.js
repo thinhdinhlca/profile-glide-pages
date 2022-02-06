3a5ef9792c259fbf110556fc57031dc41dc6e766
Thịnh Đinh <94897059+thinhdinhlca@users.noreply.github.com>
2022-02-06 10:09:55 +0700

    Update function.js

--- function.js
+++ function.js
@@ -1,91 +1,41 @@
 // https://www.chartjs.org/
 
 
-window.function = function (data, width, height, tooltips) {
+window.function = function (data) {
 
   // data
-  data = data.value ?? "100,125,45";
-  width = width.value ?? "100";
-  height = height.value ?? "80";
-  tooltips = tooltips.value ?? "";
- 
+  data = data.value ?? "";
 
   let ht = `<!DOCTYPE html>
 <html>
   <head>
     <meta charset="utf-8">
     <title>Glide Yes-Code</title>
-    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
-    <script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels@0.7.0/dist/chartjs-plugin-datalabels.min.js"></script>
+    <script src="https://cdn.anychart.com/releases/8.11.0/js/anychart-core.min.js"></script>
+    <script src="https://cdn.anychart.com/releases/8.11.0/js/anychart-scatter.min.js"></script>
   </head>
   <body>
-  <div class="chart-container" style="position: relative; height:${height}vh; width:${width}vw">
+  <div class="container" style="position: relative; height:100vh; width:80vw">
   <canvas id="gr1"></canvas>
 </div>
-    <script>
-    let config = {
-  type: 'bar',
-  data: {
-    labels: ['Socialize','Task','Inquire','Advocate','Support','Disagree','Facilitate','Action Plan'],
-    datasets: [
-      {
-        label: '',
-        backgroundColor: ['#00254D','#549F93','#E55934','#DAC4F7','#576CA8','#9A031E','#1FC7FF','#883E30'],
-        borderColor: 'black',
-        borderWidth: 1,
-        data: [${data}],
-      }
-    ],
-  },
-  options: {
-    responsive: true,
-    maintainAspectRatio: false,
-    scales: {
-        xAxes: [{
-            ticks: {
-                fontSize: 10
-            },
-            gridLines: {
-                drawOnChartArea: false
-            }
-        }],
-        yAxes: [{
-            ticks: {
-                fontSize: 12
-            },
-            gridLines: {
-                drawOnChartArea: false
-            }
-        }]
-    },
-    legend: {
-        display: false
-    },
-    title: {
-      display: true,
-      text: 'Overview by Categories (mins)',
-      fontSize: 20,
-      padding: 30
-    },
-    plugins: {
-      datalabels: {
-        display: false,
-        anchor: 'center',
-        align: 'center',
-        color: ['white','white','white','black','white','white','black','white'],
-        font: {
-          weight: 'bold',
-        },
-      },
-    },
-  },
-}
-;
+   anychart.onDocumentReady(function () {
 
-    var canv = document.getElementById('gr1');
-    var ctx = canv.getContext('2d');
-    let graph1 = new Chart(ctx, config);
-    </script>
+	// create data
+	var data = [${data}
+	];
+
+    // create a chart
+    var chart = anychart.quadrant(data);
+
+    // set the chart title
+    chart.title("Quadrant Chart: Basic Sample");
+
+    // set the container id
+    chart.container("container");
+
+    // initiate drawing the chart
+    chart.draw();
+});
   </body>
 </html>`
 
