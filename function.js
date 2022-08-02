410eb9020a9e1e825e4b4010956b093decfcc3f3
Thịnh Đinh <94897059+thinhdinhlca@users.noreply.github.com>
2022-08-02 15:22:11 +0700

    Update function.js

--- function.js
+++ function.js
@@ -48,8 +48,8 @@ window.function = function (cover,avatar) {
 
 .profile-picture {
   border-radius: 50%;
-  height: 150px;
-  width: 150px;
+  height: 10em;
+  width: 10em;
   margin-top: -75px;
   display: block;
   margin-left: auto;
